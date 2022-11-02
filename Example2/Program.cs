d43c0b417c9debcf036777a735a4d87c745aae88
Sergey <melnikov_s_a@mail.ru>
2022-11-02 16:58:03 +0300

    example2

---
+++ Example2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел
+Console.WriteLine("Введите три целых числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2 && num1>num3)
+{
+    Console.WriteLine($"Число {num1} максимальное");
+}
+else if(num2>num1 && num2>num3)
+{
+    Console.WriteLine($"Число {num2} максимальное");
+}
+else
+{
+    Console.WriteLine($"Число {num3} максимальное");
+}
